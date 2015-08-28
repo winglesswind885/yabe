e352296bdcbadd6226bdb156f6d3d57970855811
winglesswind <winglesswind885@gmail.com>
2015-08-28 14:11:01 -0500

    Add Comments into model package and add Full Test in unit test

--- app/models/Comment.java
+++ app/models/Comment.java
@@ -2,6 +2,26 @@ package models;
 
 import play.db.jpa.Model;
 
-public class Comment extends Model {
+import java.util.Date;
+import javax.persistence.*;
 
+
+@Entity
+public class Comment extends Model {
+	
+	public String author;
+	public Date postedAt;
+	
+	@Lob
+	public String content;
+	
+	@ManyToOne
+	public Post post;
+
+	public Comment(Post post, String author, String content) {
+		this.post = post;
+		this.author = author;
+		this.content = content;
+		this.postedAt = new Date();
+	}
 }
